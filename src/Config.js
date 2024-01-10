cbcca324bfdef573348c8bdc5776da12a0901555
Beast-Sanjay <sanjuniky2002@gmail.com>
2024-01-10 18:24:40 +0530

    changed the build

--- src/Config.js
+++ src/Config.js
@@ -5,12 +5,12 @@ import data from './backend/serverless/appconfig.json'
 const appConfigJson = Object.assign({}, ...data.map((x) => ({[x.OutputKey]: x.OutputValue})));
 
 const appConfig = {
-    apiGatewayInvokeUrl: 'https://3yjqgm3z4.execute-api.us-east-1.amazonaws.com/prod/' || appConfigJson.apiGatewayInvokeUrl,
-    cognitoUserPoolId: 'us-east-1_TCPQda1i' || appConfigJson.cognitoUserPoolId,
-    cognitoAppClientId: '2kuottud24v5059lftkopt0ir' || appConfigJson.cognitoAppClientId,
-    cognitoIdentityPoolId: 'us-east-1:4761e5b-330c-4be7-976e-8afd3800ecc7' || appConfigJson.cognitoIdentityPoolId,
-    appInstanceArn: 'arn:aws:chime:us-east-1:51479936173:app-instance/90968478-4f29-4443-b17b-f007ba2134c8' || appConfigJson.appInstanceArn,
+    apiGatewayInvokeUrl: 'https://a7q349et36.execute-api.us-east-1.amazonaws.com/prod/' || appConfigJson.apiGatewayInvokeUrl,
+    cognitoUserPoolId: 'us-east-1_Sd5gp2k6a' || appConfigJson.cognitoUserPoolId,
+    cognitoAppClientId: '7ussg82g7addmvscehu1bcpevb' || appConfigJson.cognitoAppClientId,
+    cognitoIdentityPoolId: 'us-east-1:55eadda0-cd33-45ec-aaa2-e880d939657e' || appConfigJson.cognitoIdentityPoolId,
+    appInstanceArn: 'arn:aws:chime:us-east-1:514799361739:app-instance/9c20dcd9-d404-44d1-989f-e2a33ced8e92' || appConfigJson.appInstanceArn,
     region: 'us-east-1',  
-    attachments_s3_bucket_name: 'f13hobbies-chatattachmentsbucket-oghaw5n9ner' || appConfigJson.attachmentsS3BucketName
+    attachments_s3_bucket_name: 'f13-community-chatattachmentsbucket-mtkrmjd2svoc' || appConfigJson.attachmentsS3BucketName
 };
 export default appConfig;
